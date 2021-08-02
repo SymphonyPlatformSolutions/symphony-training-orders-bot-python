1afdbec4d04f7721208a3609a09cef512ac387ee
Vinay Mistry <vinay@symphony.com>
2021-08-02 16:23:04 +0100

    Added Examples for REST API.

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
 import logging.config
+from os import name
 from pathlib import Path
 
 from symphony.bdk.core.activity.command import CommandContext
@@ -9,6 +10,7 @@ from symphony.bdk.core.service.datafeed.real_time_event_listener import RealTime
 from symphony.bdk.core.symphony_bdk import SymphonyBdk
 from symphony.bdk.gen.agent_model.v4_initiator import V4Initiator
 from symphony.bdk.gen.agent_model.v4_message_sent import V4MessageSent
+from symphony.bdk.gen.pod_model.v3_room_attributes import V3RoomAttributes
 
 from .activities import EchoCommandActivity, GreetUserJoinedActivity
 from .gif_activities import GifSlashCommand, GifFormReplyActivity
@@ -35,8 +37,12 @@ async def run():
         stream = await bdk.streams().create_im_or_mim([user.id])
         await bdk.messages().send_message(stream.id, "Hello IM")
 
-        # Example 2: Obtain User Information
-
+        # Example 2: Obtain User Information and send message to Room
+        room = await bdk.streams().create_room(V3RoomAttributes(name="Fancy room", description="Fancy description"))
+        logging.info("********* RoomInfo: %s", room)
+        roomId = room.room_system_info.id
+        await bdk.streams().add_member_to_room(349026222342678, roomId)
+        await bdk.messages().send_message(roomId, "Hello Room")
 
 class MessageListener(RealTimeEventListener):
     async def on_message_sent(self, initiator: V4Initiator, event: V4MessageSent):
